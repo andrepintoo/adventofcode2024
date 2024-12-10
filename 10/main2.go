36de1f4f9df3b0bf631357a0dbfe37dbebbb479f
andrepintoo <andresantospinto26@gmail.com>
2024-12-10 23:03:36 +0100

    completed day 10

---
+++ 10/main2.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+type Pos struct{ row, col int }
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	m := [][]int{}
+	trailheads := []Pos{}
+	scanner := bufio.NewScanner(file)
+	maxRow := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		l := make([]int, 0, len(line))
+		for j, c := range line {
+			n, _ := strconv.Atoi(string(c))
+			if n == 0 {
+				trailheads = append(trailheads, Pos{row: maxRow, col: j})
+			}
+			l = append(l, n)
+		}
+		m = append(m, l)
+		maxRow++
+	}
+
+	// each move can potentially lead to another 4 moves
+	totalScore := 0
+	for _, j := range trailheads {
+		totalScore += searchNines(m, j, 0)
+	}
+
+	fmt.Printf("Result: %d \n", totalScore)
+	if err = scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// returns the score from that trailhead
+func searchNines(matrix [][]int, pos Pos, currentHeight int) int {
+	rows := len(matrix)
+	cols := len(matrix[0])
+
+	if matrix[pos.row][pos.col] == 9 {
+		return 1
+	}
+
+	score := 0
+	// can go to the right, left, top or bottom
+	directions := []Pos{{0, 1}, {0, -1}, {-1, 0}, {1, 0}}
+	for _, dir := range directions {
+		nextPos := Pos{row: pos.row + dir.row, col: pos.col + dir.col}
+		if nextPos.row < 0 || nextPos.col < 0 || nextPos.row >= rows || nextPos.col >= cols {
+			continue
+		}
+		if matrix[nextPos.row][nextPos.col] == (currentHeight + 1) {
+			score += searchNines(matrix, nextPos, currentHeight+1)
+		}
+	}
+	return score
+}
